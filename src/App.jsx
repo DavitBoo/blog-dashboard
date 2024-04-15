dcef5c26e909c042364ba060ad9b17474f0b630f
DavitBoo <davidmarras@gmail.com>
2024-04-15 22:50:09 +0200

    changing react router structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,69 @@
 import CreateLabels from "./Components/CreateLabels";
 import Footer from "./Components/Footer";
 import GetPosts from "./Components/GetPosts";
+import Home from "./Components/Home";
 import Login from "./Components/Login";
 import Navbar from "./Components/Navbar";
 import NewPost from "./Components/NewPost";
 import Register from "./Components/Register";
 import SinglePost from "./Components/SinglePost";
 import GlobalStyle from "./globalStyles";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link, createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    // loader: rootLoader,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+
+      {
+        path: "/posts",
+        element: <GetPosts />,
+      },
+      {
+        path: "/labels",
+        element: <CreateLabels />,
+      },
+      {
+        path: "/new-post",
+        element: <NewPost />,
+      },
+      {
+        path: "/posts/:id",
+        element: <SinglePost />,
+      },
+    ],
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
 function App() {
   return (
-    <Router>
       <div className="App">
-        <nav>
-          <ul>
-          <li>
-              <Link to="/">Inicio</Link>
-            </li>
-            <li>
-              <Link to={`register`}>Register</Link>
-            </li>
-            <li>
-              <Link to={`posts`}>Posts</Link>
-            </li>
-            <li>
-              <Link to={`labels`}>Labels</Link>
-            </li>
-          </ul>
-        </nav>
-        <GlobalStyle />
-        <Routes>
-          <Route path="/register" element={<Register />} />
-          <Route path="/posts" element={<GetPosts />} />
-          <Route path="/labels" element={<CreateLabels />} />
-          <Route path="/" element={<NewPost />} />
-          <Route path="/posts/:id" element={<SinglePost />} />
-        </Routes>
+        <RouterProvider router={router} />
       </div>
-    </Router>
   );
 }
 
