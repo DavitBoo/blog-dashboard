44cddcfc58f3e80f0a12e3d8c28168535d5b4d01
DavitBoo <davidmarras@gmail.com>
2024-05-19 16:56:59 +0200

    data formater

--- src/Components/GetPosts.jsx
+++ src/Components/GetPosts.jsx
@@ -6,6 +6,21 @@ export default function GetPosts() {
   const [posts, setPosts] = useState([]);
   const [error, setError] = useState(null);
 
+  const formatDate = (timestamp) => {
+    const date = new Date(timestamp);
+    
+    const options = {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: false,
+    };
+  
+    return date.toLocaleDateString('es-ES', options).replace(',', ' a las');
+  };
+
   useEffect(() => {
     const fetchPosts = async () => {
       try {
@@ -35,6 +50,7 @@ export default function GetPosts() {
                 <Link to={`/posts/${post._id}`} test="hey">{post.title}</Link>
               </h2>
               <p>{post.body}</p>
+              <p>{formatDate(post.timestamp)}</p>
               { post.published ? <><p className="published">Publicado</p></> : <p className="unpublished">No publicado</p>}
             </li>
           ))}
